93c918fe9ba2d59dfb0e3f35c3eb1096e455da60
Lautaro Soffici <lautarosoffici@gmail.com>
2022-03-11 16:51:23 -0300

    refactor props

--- components/PrimaryButton/PrimaryButton.tsx
+++ components/PrimaryButton/PrimaryButton.tsx
@@ -1,20 +1,9 @@
 import { FC } from "react";
 import { Button, ButtonProps } from "@chakra-ui/react";
 
-const PrimaryButton: FC<ButtonProps> = ({ onClick, leftIcon, children }) => {
+const PrimaryButton: FC<ButtonProps> = ({ children, ...props }) => {
   return (
-    <Button
-      onClick={onClick}
-      leftIcon={leftIcon}
-      size="sm"
-      bgColor="brand.800"
-      color="white"
-      fontWeight="thin"
-      _hover={{ bgColor: "brand.750" }}
-      _active={{ bgColor: "brand.700" }}
-      _focus={{ outlineColor: "brand.700" }}
-      p={5}
-    >
+    <Button size="sm" fontWeight="thin" variant="primary" p={5} {...props}>
       {children}
     </Button>
   );
