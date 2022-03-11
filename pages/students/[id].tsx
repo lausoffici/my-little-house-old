a076dd41d84327c9e30077fa66ab630d4ff34107
Lautaro Soffici <lautarosoffici@gmail.com>
2022-03-11 15:21:34 -0300

    add student page

--- pages/students/[id].tsx
+++ pages/students/[id].tsx
@@ -1,5 +1,19 @@
-import { VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  ListItem,
+  Text,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { HiLocationMarker } from "react-icons/hi";
 
 import studentsApi from "student/api";
 import { IStudent } from "types";
@@ -9,10 +23,46 @@ type Props = {
 };
 
 const StudentDetailPage: React.FC<Props> = ({ student }) => {
+  const router = useRouter();
+  const { lastName, firstName, description, courses, email, address } = student;
+
   return (
-    <VStack w="100%" h="100vh" bgColor="brand.50" p={5}>
-      <div>{JSON.stringify(student)}</div>
-    </VStack>
+    <Box h="100vh" w="100%" p={5} bgColor="brand.50">
+      <Box bgColor="white" borderRadius="lg" p={5} position="relative" h="full">
+        <Button
+          size="sm"
+          position="absolute"
+          color="gray.600"
+          left={5}
+          variant="ghost"
+          leftIcon={<AiOutlineArrowLeft />}
+          onClick={() => router.back()}
+        >
+          Volver
+        </Button>
+        <Heading size="2xl" textAlign="center">
+          {lastName.toUpperCase()}, {firstName}
+        </Heading>
+
+        <Flex direction="column" textAlign="left" flex={1} gap={2}>
+          <Heading size="lg">Datos</Heading>
+          <Text>Nombre: {firstName}</Text>
+          <Text>Apellido: {lastName}</Text>
+          <Text>Dirección: {address}</Text>
+          <Text>Email: {email}</Text>
+          <Text>Comentarios: {description}</Text>
+          <Text>Cursos</Text>
+          <UnorderedList>
+            {courses.map((c) => (
+              <ListItem key={c}>{c}</ListItem>
+            ))}
+          </UnorderedList>
+        </Flex>
+        <Box textAlign="left" flex={1}>
+          <Heading size="lg">Cuotas</Heading>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
