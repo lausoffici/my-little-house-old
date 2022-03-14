3131578e4fcf8181542825563a90803ff84bd6e0
Julieta <julieta.pontino@gmail.com>
2022-03-14 18:11:17 -0300

    refactor and add styles

--- pages/students/[id].tsx
+++ pages/students/[id].tsx
@@ -3,17 +3,19 @@ import {
   Button,
   Flex,
   Heading,
-  HStack,
+  Divider,
   ListItem,
-  Text,
-  UnorderedList,
+  ListIcon,
+  List,
+  HStack,
   VStack,
 } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import DataText from "../../components/DataText";
 
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import { HiLocationMarker } from "react-icons/hi";
+import { AiOutlineEdit, AiOutlinePaperClip } from "react-icons/ai";
 
 import studentsApi from "student/api";
 import { IStudent } from "types";
@@ -25,6 +27,10 @@ type Props = {
 const StudentDetailPage: React.FC<Props> = ({ student }) => {
   const router = useRouter();
   const { lastName, firstName, description, courses, email, address } = student;
+
+  function capitalize(word) {
+    return String(word.charAt(0).toUpperCase() + word.slice(1));
+  }
 
   return (
     <Box h="100vh" w="100%" p={5} bgColor="brand.50">
@@ -40,27 +46,62 @@ const StudentDetailPage: React.FC<Props> = ({ student }) => {
         >
           Volver
         </Button>
-        <Heading size="2xl" textAlign="center">
+        <Heading size="2xl" textAlign="center" my={4}>
           {lastName.toUpperCase()}, {firstName}
         </Heading>
 
-        <Flex direction="column" textAlign="left" flex={1} gap={2}>
-          <Heading size="lg">Datos</Heading>
-          <Text>Nombre: {firstName}</Text>
-          <Text>Apellido: {lastName}</Text>
-          <Text>Dirección: {address}</Text>
-          <Text>Email: {email}</Text>
-          <Text>Comentarios: {description}</Text>
-          <Text>Cursos</Text>
-          <UnorderedList>
-            {courses.map((c) => (
-              <ListItem key={c}>{c}</ListItem>
-            ))}
-          </UnorderedList>
-        </Flex>
-        <Box textAlign="left" flex={1}>
-          <Heading size="lg">Cuotas</Heading>
-        </Box>
+        <VStack width="100%">
+          <Flex
+            direction="column"
+            textAlign="left"
+            flex={1}
+            gap={2}
+            bg="gray.50"
+            p={5}
+            borderRadius={10}
+            w="100%"
+          >
+            <HStack justify="space-between" align="center">
+              <Heading size="md">DATOS</Heading>
+              <Button leftIcon={<AiOutlineEdit />}>Editar</Button>
+              {/* Cambiar por buttonprimary una vez mergeada branch styles */}
+            </HStack>
+            <Divider />
+            <DataText data="Apellido: ">{capitalize(lastName)}</DataText>
+            <DataText data="Nombre: ">{capitalize(firstName)}</DataText>
+            <DataText data="Dirección: ">
+              {address ? capitalize(address) : ""}
+            </DataText>
+            <DataText data="Email: ">{email}</DataText>
+            <DataText data="Comentarios: ">
+              {description ? capitalize(description) : ""}
+            </DataText>
+            <DataText data="Cursos: ">
+              <List>
+                {courses.map((c) => (
+                  <ListItem key={c}>
+                    <ListIcon as={AiOutlinePaperClip} />
+                    {c ? capitalize(c) : ""}
+                  </ListItem>
+                ))}
+              </List>
+            </DataText>
+          </Flex>
+          <VStack
+            textAlign="left"
+            flex={1}
+            bg="gray.50"
+            p={5}
+            borderRadius={10}
+            w="100%"
+            spacing={2}
+          >
+            <HStack justify="space-between" align="center" w="100%">
+              <Heading size="md">CUOTAS</Heading>
+            </HStack>
+            <Divider />
+          </VStack>
+        </VStack>
       </Box>
     </Box>
   );
